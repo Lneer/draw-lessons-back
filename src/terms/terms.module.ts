3aff66ea73791b2bf13a4303b04f137a9049c0f0
Lneer <civil_a@list.ru>
2025-08-18 19:30:27 +0300

    feat: init project

--- src/terms/terms.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { TermsService } from './terms.service';
-import { TermsController } from './terms.controller';
-
-@Module({
-  controllers: [TermsController],
-  providers: [TermsService],
-})
-export class TermsModule {}
